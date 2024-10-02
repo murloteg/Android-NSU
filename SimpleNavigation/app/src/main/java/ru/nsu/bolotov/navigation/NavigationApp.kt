da29719340e20e9999043514dfa693e237568556
Kirill Bolotov <k.y.bolotov@gmail.com>
2024-10-02 16:58:26 +0700

    Убрал неиспользуемый Column-контейнер

--- SimpleNavigation/app/src/main/java/ru/nsu/bolotov/navigation/NavigationApp.kt
+++ SimpleNavigation/app/src/main/java/ru/nsu/bolotov/navigation/NavigationApp.kt
@@ -1,8 +1,6 @@
 package ru.nsu.bolotov.navigation
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -20,11 +18,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -77,13 +73,6 @@ fun NavigationApp(
     val currentScreen = Screens.valueOf(
         backStackEntry?.destination?.route ?: Screens.Start.name
     )
-
-    Column {
-        Column(modifier = Modifier) {
-            Text("First Screen", modifier.background(Color.Green).padding(40.dp))
-        }
-        Column {  }
-    }
 
     Scaffold (
         topBar = {
